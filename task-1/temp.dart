712a3415623324d5b7e8142444401f34d689045a
yasmine <jasminamgad@gmail.com>
2026-03-11 20:12:28 +0200

    Cleanup for re-upload

--- task-1/temp.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  int c = 27;
-  double f = c * 9 / 5 + 32;
-
-  print(
-    "Temperature in celsius is ${c}" + " \nTemperature in fahrenheit is ${f}",);
-
-}
--- task-2/square.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  List<int> nums = [1, 2, 2];
-  int sum = 0;
-  for (int i = 0; i < nums.length; i++) {
-    sum += nums[i] * nums[i];
-  }
-  print(sum);
-}

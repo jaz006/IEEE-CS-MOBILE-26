4779ecbb7de7bc3d653146f82a31b4d0d2cb857e
yasmine <jasminamgad@gmail.com>
2026-03-11 20:03:18 +0200

    clean up files to reorganize

--- task-1/convert.dart
+++
@@ -1,5 +0,0 @@
-void main() {
-  String x1 = "405";
-  String x2 = "405.6";
-  print("integer = ${int.parse(x1)}\n" + "double = ${double.parse(x2)}");
-}
--- task-3/dryingpotatoes.dart
+++
@@ -1,9 +0,0 @@
-int potatoes(int p0, int w0, int p1) {
-  double dryWeight = w0 * (100 - p0) / 100;
-  double finalWeight = dryWeight * 100 / (100 - p1);
-  return finalWeight.toInt();
-}
-
-void main() {
-  print(potatoes(99, 100, 98)); 
-}
